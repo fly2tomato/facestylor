3374f42bc1b558ff436720ae16faf984cd3afff7
fly2tomato <xuju5220@gmail.com>
2023-11-06 18:29:04 +0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -106,6 +106,24 @@ def faceStylor(img, style_label, with_qrcode=True):
       testor.move_out_cuda()
       return "failed"
 
+
+def exe_shell(command: str, shell: str = '/bin/bash'):
+    return Popen(command, stdout=PIPE, stderr=STDOUT, shell=True, executable=shell)
+
+
+process=exe_shell(command="echo 'start to run shell script' && bash run_streamlit.sh")
+try:
+    with (process.stdout):
+        y = 0
+        for line in iter(process.stdout.readline, b''):
+            # s = str(line).replace("b'", "").replace("'", "").replace("\\n", "")
+            s = line.decode('utf-8')
+            print(s)
+except Exception as e:
+    print(e)
+
+
+
 base_root = os.path.abspath(os.path.join(__file__, "../"))
 _QR_PATH = base_root + "/qrcode.png"
 
