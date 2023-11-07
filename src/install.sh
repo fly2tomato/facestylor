952c8fd5dc9e166c9d4bda7567d21d9458b912bb
fly2tomato <xuju5220@gmail.com>
2023-11-07 13:32:59 +0800

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -3,9 +3,6 @@
 pip install -U openmim
 mim install mmcv-full==1.7.1
 
-git clone https://github.com/open-mmlab/mmgeneration.git
 cd mmgeneration
 pip install -r requirements.txt
 pip install -v -e .
-
-pip install mmcls
